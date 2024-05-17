85eda8d6fd77bf92268145b74a58fd5024f7725e
DonYall <highvoltage9886@gmail.com>
2024-05-17 00:10:11 -0400

    revamp nav & homepage, start contact page

--- scripts/header.js
+++ scripts/header.js
@@ -3,9 +3,9 @@ const navUnderline = document.getElementById("nav-underline");
 
 for (let i = 0; i < navTabs.length; i++) {
     navTabs[i].addEventListener("mouseover", function () {
-        navUnderline.style.left = `${i * 200}px`;
+        navUnderline.style.left = `${i * 150}px`;
     });
     navTabs[i].addEventListener("mouseout", function () {
-        navUnderline.style.left = `${navIndex * 200}px`;
+        navUnderline.style.left = `${navIndex * 150}px`;
     });
 }
