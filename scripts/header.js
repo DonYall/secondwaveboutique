cfb33b612b09133ede8be0a00fac22e1b49505b0
DonYall <highvoltage9886@gmail.com>
2024-05-20 00:08:48 -0400

    dark mode wip

--- scripts/header.js
+++ scripts/header.js
@@ -14,3 +14,16 @@ function toggleNav() {
     document.querySelector("nav").classList.toggle("active");
     document.getElementById("hamburger").classList.toggle("active");
 }
+
+document.getElementById("toggle-theme").addEventListener("click", function () {
+    const r = document.querySelector(":root");
+    this.classList.toggle("active");
+    const light = !this.classList.contains("active");
+    r.style.setProperty("--color-background", light ? "#fff" : "#1a1a1a");
+    r.style.setProperty("--color-text", light ? "#000" : "#fff");
+    r.style.setProperty("--color-text-brighter", light ? "#434343" : "#d9d9d9");
+    r.style.setProperty("--button-background", light ? "#202020" : "#fde9ed");
+    r.style.setProperty("--button-text", light ? "#fff" : "#000");
+    r.style.setProperty("--button-hover", light ? "#fde9ed" : "#202020");
+    document.getElementById("hero-logo").src = light ? "../assets/img/logo.png" : "../assets/img/logo-darkmode.png";
+});
